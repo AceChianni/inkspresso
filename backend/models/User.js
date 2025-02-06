46af86fa11616849293c15d99d14213695eb7297
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2025-02-06 19:07:51 +0000

    update user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
-    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
@@ -12,6 +11,9 @@ const userSchema = mongoose.Schema(
     verificationToken: { type: String },
     resetPasswordToken: { type: String },
     resetPasswordExpires: { type: Date },
+
+    // Admin flag
+    isAdmin: { type: Boolean, default: false },
 
     // Wishlist and checked out books
     wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
@@ -35,8 +37,6 @@ const User = mongoose.model("User", userSchema);
 
 module.exports = User;
 
-
-
 // const mongoose = require("mongoose");
 // const bcrypt = require("bcryptjs");
 
@@ -50,17 +50,21 @@ module.exports = User;
 //     verificationToken: { type: String },
 //     resetPasswordToken: { type: String },
 //     resetPasswordExpires: { type: Date },
+
+//     // Wishlist and checked out books
+//     wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
+//     checkedOutBooks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
 //   },
 //   { timestamps: true }
 // );
 
-// // Password hashing before saving
+// // Hash password before saving
 // userSchema.pre("save", async function (next) {
 //   if (!this.isModified("password")) return next();
 //   this.password = await bcrypt.hash(this.password, 10);
 // });
 
-// // Method to compare entered password with hashed password
+// // Compare entered password with hashed password
 // userSchema.methods.matchPassword = async function (password) {
 //   return await bcrypt.compare(password, this.password);
 // };
