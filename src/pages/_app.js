6acabe11502b49856a462feb0d547c74e4baacfd
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2025-11-11 23:05:13 +0000

    update styles to show nav hover

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 // /src/pages/_app.js
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CartProvider } from "@/context/CartContext";
 import Navbar from "@/components/Navbar";
 import CartDrawer from "@/components/CartDrawer";
@@ -11,15 +10,19 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   const [cartOpen, setCartOpen] = useState(false);
 
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", "inkspresso");
+  }, []);
+
   return (
     <CartProvider>
       <FavoritesProvider>
-      <Navbar toggleCart={() => setCartOpen(!cartOpen)} />
-      <CartDrawer isOpen={cartOpen} toggleCart={() => setCartOpen(false)} />
-      <main className="pt-20">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
+        <Navbar toggleCart={() => setCartOpen(!cartOpen)} />
+        <CartDrawer isOpen={cartOpen} toggleCart={() => setCartOpen(false)} />
+        <main className="pt-20">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
       </FavoritesProvider>
     </CartProvider>
   );
